ff491891b17e9defdf72c426271efcc587542221
Francesco170 <118993493+Francesco170@users.noreply.github.com>
2023-01-21 03:09:14 +0100

    Add files via upload

---
+++ es9.js
@@ -0,0 +1,10 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+let keys= Object.keys(person);
+let value= Object.values(person);
+console.log(keys,value);
+
+// Print values of person using Object.keys
